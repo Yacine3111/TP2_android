d6ed825b53d5bf57b421f6a910865045bf233952
YACINE BOUREZAK <2288656@cegepgranby.qc.ca>
2025-02-26 22:48:21 -0500

    other scheme search, home page, new home page, show url on search

--- app/src/main/java/com/example/tp2_android/MainActivity.java
+++ app/src/main/java/com/example/tp2_android/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.tp2_android;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +20,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     SearchView searchBar;
-    ImageButton navigateBtn;
+    ImageButton navigateBtn,nextBtn,previousBtn,homeBtn,newHomeBtn;
     WebView webView;
+    String homePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,36 +38,70 @@ public class MainActivity extends AppCompatActivity {
 
         searchBar=findViewById(R.id.search_bar);
         navigateBtn=findViewById(R.id.navigate_btn);
+        homeBtn=findViewById(R.id.home_btn);
+        previousBtn=findViewById(R.id.previous_btn);
+        nextBtn=findViewById(R.id.next_btn);
+        newHomeBtn=findViewById(R.id.new_home_btn);
         webView=findViewById(R.id.web_view);
 
+
         webView.getSettings().setJavaScriptEnabled(true);
+
+        homePath="https://www.google.com";
+        webView.loadUrl(homePath);
+
+
+        navigateBtn.setOnClickListener(v->{
+            Uri uri= Uri.parse(searchBar.getQuery().toString());
+            checkUrl(webView,uri);
+        });
+
+        homeBtn.setOnClickListener(v->{
+            webView.loadUrl(homePath);
+        });
+
+        newHomeBtn.setOnClickListener(v->{
+            homePath=searchBar.getQuery().toString();
+            alertMessage(getString(R.string.message_new_home_page));
+        });
 
 
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
                 Uri uri=request.getUrl();
-                String scheme=uri.getScheme();
-                if(scheme==null){
-                    view.loadUrl("https://"+uri);
-                    return false;
-                }else if(scheme.equals("http")||scheme.equals("https")){
-                    return false;
-                }else{
-
-                    return true;
-                }
+                return checkUrl(view,uri);
+            }
+            @Override
+            public void onPageFinished(WebView view,String url){
+                searchBar.setQuery(url,false);
             }
         });
 
-        webView.loadUrl("google.ca");
 
+    }
 
-        navigateBtn.setOnClickListener(v->{
-            webView.loadUrl(searchBar.getQuery().toString());
-        });
-
-
-
+    public boolean checkUrl(WebView view,Uri uri){
+        String scheme=uri.getScheme();
+        String pagePath = uri.toString();
+        if(scheme==null) {
+            pagePath="https://" + uri;
+            view.loadUrl(pagePath);
+            return false;
+        } else if (scheme.equals("http")||scheme.equals("https")) {
+            view.loadUrl(pagePath);
+            return false;
+        }else{
+            Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+            try{
+                startActivity(intent);
+            }catch (ActivityNotFoundException e){
+                alertMessage(getString(R.string.message_no_app_found)+" "+scheme);
+            }
+            return true;
+        }
+    }
+    public void alertMessage(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
